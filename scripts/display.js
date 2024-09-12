36ccb82d736a446b03ea7a72350eb1a2fa8dd6ed
SamanthaSDGKU <48604801+SamanthaSDGKU@users.noreply.github.com>
2024-09-11 19:56:56 -0700

    deleting and search function

--- scripts/display.js
+++ scripts/display.js
@@ -11,3 +11,27 @@ function displayCards(){
         `
     }
 }
+
+function displayRows(){
+    //get the tbody
+    const TABLE = document.getElementById("petRows");
+    // clear the tbody
+    let row="";
+    //travel the array (for)
+    for(let i=0;i<pets.length;i++){
+        let pet = pets[i];
+        //create the template row
+        row+=`
+            <tr id="${i}">
+                <td>${pet.name}</td>
+                <td>${pet.age}</td>
+                <td>${pet.gender}</td>
+                <td> <button class="btn btn-sm btn-danger" onclick="deletePet(${i})"> Delete </button></td>
+            </tr>
+        `;
+    }
+    TABLE.innerHTML=row;
+    document.getElementById("totalPets").innerHTML=pets.length;
+}
+
+//*** on the register.js replace the displayCards() by displayRows() */
--- scripts/register.js
+++ scripts/register.js
@@ -1,34 +1,83 @@
 let pets=[];//empty array
 //create the constructor
 //<------ paramenters (local) --->
-function Pet(name,age,gender,breed){
+function Pet(name,age,gender,breed,service){
     //property=paramenter
     this.name=name;
     this.age=age;
     this.gender=gender;
     this.breed=breed;
+    this.service=service;
 }
 //get the inputs
 let inputName = document.getElementById("txtName");
 let inputAge = document.getElementById("txtAge");
-let inputGender = document.getElementById("txtGender");
 let inputBreed = document.getElementById("txtBreed");
+let inputService=document.getElementById("txtService");
+
+function isValid(pet){
+    let validation =true;
+    //check if name is empty?
+    if(pet.name==""){
+        validation=false;
+    }
+    return validation;
+}
 
 function register(){
+    let inputGender = document.querySelector('input[name="gender"]:checked');
+    console.log(inputGender.value);
+
     console.log(inputName.value,inputAge.value,inputGender.value,inputBreed.value);
     //create an obj
-    let newPet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value);
-    pets.push(newPet)//push the newpet to the array
-    //display on the console the new pet  
-    console.log(newPet);
-    displayCards();
+    let newPet = new Pet(inputName.value,inputAge.value,inputGender.value,inputBreed.value,inputService.value);
+
+    if(isValid(newPet)==true){
+        pets.push(newPet)//push the newpet to the array
+        //display on the console the new pet  
+        console.log(newPet);
+        // displayCards();
+        displayRows();
+    }else{
+        alert("Fill the information");
+    }
+}
+
+function deletePet(petID){
+    console.log("Deleting the pet");
+    document.getElementById(petID).remove();//deletes from the HTML
+    pets.splice(petID,1);//removes the pet from the array
+    displayRows();
+}
+
+function search(){
+    //by name
+    displayRows();
+    //display on the console the text that user enters on the input
+    let ss=document.getElementById("txtSearch").value;
+    let foundPet=null;
+    let id;
+
+    for(let i=0;i<pets.length;i++){
+        if(pets[i].name.toLowerCase() == ss.toLowerCase()){
+            foundPet=pets[i];
+            id=i;
+            break;
+        }
+    }
+    if(foundPet){
+        document.getElementById(id).classList.add("highlight");
+    }else{
+        displayRows();
+    }
 }
 
 function init(){
     console.log("Init fn");
-    let scooby = new Pet("Scooby",60,"Male","Dane");
-    let scrappy= new Pet("Scrappy",50,"Male","Boxer");
+    let scooby = new Pet("Scooby",60,"Male","Dane","grooming");
+    let scrappy= new Pet("Scrappy",50,"Male","Boxer","vaccines");
     pets.push(scooby,scrappy);
-    displayCards();
+    // displayCards();
+    displayRows();
 }
 window.onload=init;//it waits to render the HMTL
